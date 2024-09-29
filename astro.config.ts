a350b74274dc3c24b62975fa7cac2a63bd40cc19
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-09-29 16:02:30 +0000

    setup tailwind / react

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig, type AstroUserConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })]
 }) satisfies AstroUserConfig;
