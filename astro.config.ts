4925d7ab0f7e4d5095802e6589ba8541c0a19578
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-09-29 18:05:21 +0000

    add NavBar

--- astro.config.ts
+++ astro.config.ts
@@ -4,9 +4,11 @@ import { defineConfig, type AstroUserConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  })]
+  }), icon()]
 }) satisfies AstroUserConfig;
